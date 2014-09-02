82912982f8d04c5df993382df57c0ec98b7e2935
Reuben Villagomez <beholdasun@gmail.com>
2014-09-02 15:25:14 -0400

    Assignment 1
    Three questions to verify that I’ve installed R, RStudio, and
    PostgreSQL.

---
+++ Assignment1.R
@@ -0,0 +1,12 @@
+# 1. What is the version of R and RStudio do you have installed?
+answer.1 <- "R Version R 3.1.1 GUI 1.65 Mavericks build (6784) and 
+             RStudio version 0.98.1028"
+
+# 2. What version of PostgreSQL do you have installed?
+answer.2 <- "PostgreSQL Version 9.3"
+
+# 3. Install and load the R package DMwR. Load the data set sales and 
+#    determine the number of observations contained in the data set.
+answer.3 <- "There are 401,146 transaction reports, each with 5 pieces
+             of data (sales ID, product ID, quantity, value of transaction,
+             and Insp). The answer is either 401,146 or 2,005,730."
